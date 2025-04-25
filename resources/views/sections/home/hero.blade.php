0b2c4d3cdae002734fa6e90738fb6559ffca4abe
Felipe Vega <felipevega.dev@gmail.com>
2025-04-25 18:26:14 -0400

    PERFECTO

--- resources/views/sections/home/hero.blade.php
+++ resources/views/sections/home/hero.blade.php
@@ -1,7 +1,7 @@
 <!-- Hero Section -->
-<section class="relative bg-[#362766] pt-0 overflow-x-hidden max-w-full">
+<section class="relative bg-[#362766] pt-0 overflow-hidden w-full">
   <!-- Video de fondo con overlay -->
-  <div class="absolute inset-0 z-0">
+  <div class="absolute inset-0 z-0 w-full h-full">
     <div class="absolute inset-0 bg-[#362766] opacity-70 z-10"></div>
     <video 
       class="absolute inset-0 w-full h-full object-cover" 
@@ -31,8 +31,8 @@
   </div>
 
   <!-- Curvatura inferior -->
-  <div class="absolute bottom-0 left-0 right-0 z-10 translate-y-1">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" fill="#ffffff" preserveAspectRatio="none">
+  <div class="absolute bottom-0 left-0 right-0 z-10">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" fill="#ffffff" class="w-full" preserveAspectRatio="none">
       <path d="M0,64L80,69.3C160,75,320,85,480,80C640,75,800,53,960,48C1120,43,1280,53,1360,58.7L1440,64L1440,120L1360,120C1280,120,1120,120,960,120C800,120,640,120,480,120C320,120,160,120,80,120L0,120Z"></path>
     </svg>
   </div>
