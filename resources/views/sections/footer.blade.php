1a39ad3d0ea262dcc67c24cae0b8b5509006d1cf
Felipe Vega <felipevega.dev@gmail.com>
2025-04-27 22:32:59 -0400

    footer

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -3,10 +3,10 @@
   <div class="container mx-auto relative px-4" style="max-width: 1512px;">
     <div class="grid grid-cols-1 md:grid-cols-12 gap-8 md:gap-6 relative">
       <!-- Columna 1: Logo y redes sociales -->
-      <div class="flex flex-col items-center md:items-start pl-8 md:pl-12 md:col-span-4">
+      <div class="flex flex-col items-center md:items-start pl-8 md:pl-12 md:col-span-3">
         <!-- Logo -->
-        <div class="mb-8">
-          <img src="{{ get_theme_file_uri('resources/images/kintsugi-hero.png') }}" alt="Logo" class="w-40 h-auto">
+        <div class="mb-6">
+          <img src="{{ get_theme_file_uri('resources/images/kintsugi-hero-azul.png') }}" alt="Logo" class="w-40 h-auto">
         </div>
         
         <!-- Siguenos -->
@@ -15,7 +15,7 @@
         </h3>
         
         <!-- Iconos sociales -->
-        <div class="flex space-x-8 items-center mb-6">
+        <div class="flex space-x-6 items-center mb-6">
           <a href="https://www.instagram.com/instituto_kintsugi/" target="_blank" class="text-[#181818] hover:text-[#AB277A] transition-colors duration-300">
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
               <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
@@ -37,17 +37,10 @@
             </svg>
           </a>
         </div>
-        
-        <!-- Frase inspiradora -->
-        <div class="text-[#AB277A] text-2xl md:text-3xl text-center md:text-left w-full" style="font-family: 'Hugamour', sans-serif;">
-          <p class="mb-0">#El Trauma</p>
-          <p class="mb-0 md:ml-8">Se Puede</p>
-          <p class="mb-0 md:ml-16">Superar</p>
-        </div>
       </div>
       
       <!-- Columna 2: Contacto -->
-      <div class="flex flex-col items-center md:items-start md:mt-[80px] md:col-span-2">
+      <div class="flex flex-col items-center md:items-start md:mt-0 md:col-span-2">
         <h3 class="text-[#AB277A] font-medium mb-4 text-xl" style="font-family: 'Roboto', sans-serif;">
           Contacto
         </h3>
@@ -61,7 +54,7 @@
       </div>
       
       <!-- Columna 3: Privacidad -->
-      <div class="flex flex-col items-center md:items-start md:mt-[80px] md:col-span-2">
+      <div class="flex flex-col items-center md:items-start md:mt-0 md:col-span-2">
         <h3 class="text-[#AB277A] font-medium mb-4 text-xl" style="font-family: 'Roboto', sans-serif;">
           Privacidad
         </h3>
@@ -78,13 +71,23 @@
           </li>
         </ul>
       </div>
+
+      <!-- Columna 4: Frase -->
+      <div class="flex flex-col items-center md:items-start md:mt-0 md:col-span-2">
+        <!-- Frase inspiradora -->
+        <div class="text-[#AB277A] text-2xl md:text-3xl text-center md:text-left" style="font-family: 'Hugamour', sans-serif;">
+          <p class="mb-0">#El Trauma</p>
+          <p class="mb-0 md:ml-8">Se Puede</p>
+          <p class="mb-0 md:ml-16">Superar</p>
+        </div>
+      </div>
       
-      <!-- Columna 4: Instituciones Asociadas -->
-      <div class="flex flex-col items-center md:items-start md:mt-[80px] md:col-span-4">
+      <!-- Columna 5: Instituciones Asociadas -->
+      <div class="flex flex-col items-center md:items-start md:mt-0 md:col-span-3">
         <h3 class="text-[#AB277A] font-medium mb-4 text-xl" style="font-family: 'Roboto', sans-serif;">
           Instituciones Asociadas
         </h3>
-        <div class="flex flex-row items-center space-x-6">
+        <div class="grid grid-cols-2 gap-4">
           <!-- Logo 1 -->
           <div>
             <a href="https://www.emdr-es.org/Terapeutas" target="_blank" class="transition-opacity duration-300 hover:opacity-80">
@@ -95,6 +98,18 @@
           <div>
             <a href="https://emdrchile.cl/terapeutas/julio-cesar-carrasco-rebolledo/" target="_blank" class="transition-opacity duration-300 hover:opacity-80">
               <img src="{{ get_theme_file_uri('resources/images/logoemdr2.png') }}" alt="EMDR Chile" class="w-24 h-auto">
+            </a>
+          </div>
+          <!-- Logo 3 -->
+          <div>
+            <a href="https://www.happinessstudies.academy/" target="_blank" class="transition-opacity duration-300 hover:opacity-80">
+              <img src="{{ get_theme_file_uri('resources/images/logo-hsa.avif') }}" alt="Happiness Studies Academy" class="w-28 h-auto">
+            </a>
+          </div>
+          <!-- Logo 4 -->
+          <div>
+            <a href="https://www.psicologiadeldeportechile.cl/" target="_blank" class="transition-opacity duration-300 hover:opacity-80">
+              <img src="{{ get_theme_file_uri('resources/images/logo-psidepchile.avif') }}" alt="Sociedad Chilena de Psicología del Deporte" class="w-24 h-auto">
             </a>
           </div>
         </div>
